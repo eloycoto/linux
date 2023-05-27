15afb4a7e6c691b4f70294c94991066b950b6bfe
Asahi Lina <lina@asahilina.net>
2023-05-27 19:38:57 +0900

    rust: sync: Replace static LockClassKey refs with a pointer wrapper
    We want to be able to handle dynamic lock class creation and using
    pointers to things that aren't a real lock_class_key as lock classes.
    Doing this by casting around Rust references is difficult without
    accidentally invoking UB.
    
    Instead, switch LockClassKey to being a raw pointer wrapper around a
    lock_class_key, which means there is no UB possible on the Rust side
    just by creating and consuming these objects. The C code also should
    never actually dereference lock classes, only use their address
    (possibly with an offset).
    
    We still provide a dummy ZST version of this wrapper, to be used when
    lockdep is disabled.
    
    Signed-off-by: Asahi Lina <lina@asahilina.net>
    (cherry picked from commit 1f502564c869c5b6a861bca4924ff039cbe9255b)
    Signed-off-by: Fabien Parent <fabien.parent@linaro.org>

--- rust/kernel/sync/poll.rs
+++ rust/kernel/sync/poll.rs
@@ -85,7 +85,7 @@ pub struct PollCondVar {
 
 impl PollCondVar {
     /// Constructs a new condvar initialiser.
-    pub fn new(name: &'static CStr, key: &'static LockClassKey) -> impl PinInit<Self> {
+    pub fn new(name: &'static CStr, key: LockClassKey) -> impl PinInit<Self> {
         pin_init!(Self {
             inner <- CondVar::new(name, key),
         })
