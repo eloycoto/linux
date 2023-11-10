7754607c1b8ac9ce1e7cb3e9a18c5e60f5f4980d
Fabien Parent <fabien.parent@linaro.org>
2023-11-10 11:15:15 -0800

    IMPORT: rust: port of api from 'rust' branch
    Signed-off-by: Fabien Parent <fabien.parent@linaro.org>

--- rust/helpers.c
+++ rust/helpers.c
@@ -29,6 +29,7 @@
 #include <linux/errname.h>
 #include <linux/fs.h>
 #include <linux/mutex.h>
+#include <linux/of_device.h>
 #include <linux/refcount.h>
 #include <linux/sched/signal.h>
 #include <linux/security.h>
@@ -263,6 +264,13 @@ const char *rust_helper_dev_name(const struct device *dev)
 	return dev_name(dev);
 }
 EXPORT_SYMBOL_GPL(rust_helper_dev_name);
+
+const struct of_device_id *rust_helper_of_match_device(
+		const struct of_device_id *matches, const struct device *dev)
+{
+	return of_match_device(matches, dev);
+}
+EXPORT_SYMBOL_GPL(rust_helper_of_match_device);
 
 /*
  * `bindgen` binds the C `size_t` type as the Rust `usize` type, so we can
--- rust/kernel/lib.rs
+++ rust/kernel/lib.rs
@@ -43,6 +43,7 @@ pub mod ioctl;
 pub mod kunit;
 #[cfg(CONFIG_NET)]
 pub mod net;
+pub mod of;
 pub mod prelude;
 pub mod print;
 pub mod security;
